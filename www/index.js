8fa483c324abd57dab21d49f606ea27ab8440f97
J B <J B>
2017-02-13 02:38:34 -0800

    Adds in a way to run several types of reports at once

--- www/index.js
+++ www/index.js
@@ -1,45 +1,43 @@
-#!/usr/bin/env babel-node
+// #!/usr/bin/env babel-node
 
 const google = require('googleapis');
 const config = require('../config');
+const settings = require('./settings');
 
 const key = config.googleAuth;
+const reports = settings.reports;
 const views = config.gaViews;
 const VIEW_ID = views.basis;
 
-const jwtClient = new google.auth.JWT(
-	key.client_email,
-	null,
-	key.private_key, ['https://www.googleapis.com/auth/analytics.readonly'],
-	null
-);
+const jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, ['https://www.googleapis.com/auth/analytics.readonly'], null);
 
-jwtClient.authorize((err, tokens) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	const analytics = google.analytics('v3');
-	queryData(analytics);
+jwtClient.authorize((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    const analytics = google.analytics('v3');
+    reports.map((r) => {
+        queryData(analytics, r);
+    });
 });
 
-
-function queryData(analytics) {
-	analytics.data.ga.get({
-		auth: jwtClient,
-		ids: VIEW_ID,
-		metrics: 'ga:uniquePageviews',
-		dimensions: 'ga:pagePath',
-		'start-date': '30daysAgo',
-		'end-date': 'yesterday',
-		sort: '-ga:uniquePageviews',
-		'max-results': 10,
-		filters: 'ga:pagePath=~/ch_[-a-z0-9]+[.]html$',
-	}, (err, response) => {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log(JSON.stringify(response, null, 4));
-	});
+function queryData(analytics, reports, dates) {
+    analytics.data.ga.get({
+        auth: jwtClient,
+        ids: VIEW_ID,
+        dimensions: reports.dimensions,
+        metrics: reports.metrics,
+        'start-date': '2015-11-01',
+        'end-date': '2015-11-01',
+        sort: '-ga:sessions',
+        // 'max-results': 50,
+    }, (err, response) => {
+        if (err) {
+            // console.log('ERROR: ' + err.errors[0].message);
+            console.log(err);
+            return;
+        }
+        console.log(JSON.stringify(response, null, 4));
+    });
 }
---
+++ www/settings.js
@@ -0,0 +1,6 @@
+exports.reports = [
+    {
+        dimensions: 'ga:channelGrouping',
+        metrics: 'ga:sessions,ga:users,ga:newUsers'
+    }
+];
